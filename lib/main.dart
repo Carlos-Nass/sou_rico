05832c7c91c061bf60199fe427c255d0ff5c220d
Carlos Eduardo Nass <carlos.nass@byseven.com.br>
2022-08-10 21:59:10 -0300

    Atualizado

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.black,
         appBar: AppBar(
           title: const Text('Sou Rico'),
-          backgroundColor: Colors.black54,
+          backgroundColor: Colors.green,
         ),
         body: const Center(
           child: Image(
